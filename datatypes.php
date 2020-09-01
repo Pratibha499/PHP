55e5096266395cfd7c1a4e86e3f1711e96cb8bad
pratibha sharma <54215583+Pratibha499@users.noreply.github.com>
2020-09-01 09:35:33 -0400

    Data Types in php

---
+++ datatypes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $str="Hello World";
+    echo $str;
+    echo "<br>";
+    $int=65765;
+    print $int;
+    echo "<br>";
+    $boolean=true;
+    echo $boolean;
+    echo "<br>";
+    $float="6576.65";
+    print $float;
+    echo "<br>";
+    $array=array("me","you","they","he","she","it");
+    var_dump($array);
+    ?>
+</body>
+</html>
