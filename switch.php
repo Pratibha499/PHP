107bc54a193fb7cff1e7a3b8af79bbb7e352e5ce
pratibha sharma <54215583+Pratibha499@users.noreply.github.com>
2020-09-01 12:00:47 -0400

    switch

---
+++ switch.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$num=10;
+switch($num){
+    case 1:
+        echo "1";
+        break;
+    case 2:
+        echo "2";
+        break;
+    case 3:
+        echo "3";
+        break;
+    case 4:
+        echo "4";
+        break;
+     case 5:
+        echo "5";
+        break;
+    default:
+    echo "no data found";
+
+}
+
+
+
+
+    ?>
+</body>
+</html>
