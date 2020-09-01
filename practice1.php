6095d50ac4e7e5c8b4d267521e308740e04272c2
pratibha sharma <54215583+Pratibha499@users.noreply.github.com>
2020-09-01 09:19:43 -0400

    Basics of php

---
+++ practice1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Welcome to my World";
+    echo "<br>";
+    $x=6;
+    $y=10;
+    echo $x + $y;
+    echo "<br>";
+    $text="PHP";
+    echo "I love" .$text. "!";
+    ?>
+</body>
+</html>
