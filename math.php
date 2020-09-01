9906ee62d870af73aeafa4a6f11187f9dfd7fa9b
pratibha sharma <54215583+Pratibha499@users.noreply.github.com>
2020-09-01 11:19:16 -0400

    Basics of Math in php

---
+++ math.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo (pi());
+    echo "<br>";
+    echo (min(78,-14,89,-665));
+    echo "<br>";
+    echo (max(87,873232,97458,76375682));
+    echo "<br>";
+    echo (abs(6346.675));
+    echo "<br>";
+    echo(sqrt(4));
+
+    echo "<br>";
+    echo(round(8.44));
+    echo "<br>";
+    echo (rand());
+    echo "<br>";
+    echo (rand(5,10));
+    
+    ?>
+</body>
+</html>
