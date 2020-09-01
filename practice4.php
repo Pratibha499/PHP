de496a28aeec3ba191e360d32140d6564c545010
pratibha sharma <54215583+Pratibha499@users.noreply.github.com>
2020-09-01 09:23:28 -0400

    function var_dump()

---
+++ practice4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a=6876487;
+    $b=647.776;
+    var_dump($a);         // this function returns the value with their Data types
+    echo "<br>";
+    var_dump($b);
+    ?>
+</body>
+</html>
